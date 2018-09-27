b7d9d8e89923ed68b1e607d15224ddd7472ab72e
Xinyi Zhang <xinzhang@e1z3r3p5.42.us.org>
2018-09-26 23:06:54 -0700

    add

--- stat_info.c
+++ stat_info.c
@@ -6,7 +6,7 @@
 /*   By: xinzhang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/23 21:56:06 by xinzhang          #+#    #+#             */
-/*   Updated: 2018/09/26 23:01:18 by xinzhang         ###   ########.fr       */
+/*   Updated: 2018/09/26 23:05:32 by xinzhang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,31 @@ void stat_info(stat_dlist *dlist, char *fn)
 //	printf("in stat_info\n");
 	DIR *d;
 	struct dirent *dir;
-	s_stat **infoptr;
+	s_stat *infoptr;
 	char *dname;
 	s_passwd	*psd;
 	s_group		*grp;
 	char *usrname;
 	char *gname;
  
-	infoptr = (s_stat **)malloc(sizeof(s_stat *));
-	*infoptr = (s_stat *)malloc(sizeof(s_stat));
+//	infoptr = (s_stat **)malloc(sizeof(s_stat *));
+//	*infoptr = (s_stat *)malloc(sizeof(s_stat));
 	d = opendir(fn);
 	if (d)
 	{
 	while ((dir = readdir(d)) != NULL)
 	{
-		*infoptr = (s_stat *)malloc(sizeof(struct stat));
+		infoptr = (s_stat *)malloc(sizeof(struct stat));
 		
 		dname = strdup(dir->d_name);
-		lstat(dir->d_name, *infoptr);
-		psd = getpwuid((*infoptr)->st_uid);
+		lstat(dir->d_name, infoptr);
+		psd = getpwuid(infoptr->st_uid);
 		usrname = strdup(psd->pw_name);
-		grp = getgrgid((*infoptr)->st_gid);
+		grp = getgrgid(infoptr->st_gid);
 		gname = strdup(grp->gr_name);
 
-		dlist = appendnode(dlist, *infoptr, dname, usrname, gname);
-		dlist->totalsize += (*infoptr)->st_blocks;
+		dlist = appendnode(dlist, infoptr, dname, usrname, gname);
+		dlist->totalsize += infoptr->st_blocks;
 	}
 	closedir(d);
 	}
